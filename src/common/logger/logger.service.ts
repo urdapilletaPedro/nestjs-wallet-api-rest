84c7bf56b30d7894762c120fb0b0442a351b9676
Pedro Urdapilleta <pedrourda03@gmail.com>
2025-05-28 13:54:34 -0300

    console logger with winston

--- src/common/logger/logger.service.ts
+++ src/common/logger/logger.service.ts
@@ -1,19 +1,43 @@
 import { Injectable, LoggerService } from '@nestjs/common';
-import { createLogger, format, transports } from 'winston';
+import {
+  createLogger,
+  format,
+  transports,
+  Logger as WinstonLogger,
+} from 'winston';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Injectable()
 export class AppLogger implements LoggerService {
-  private readonly logger = createLogger({
-    format: format.combine(
-      format.timestamp(),
-      format.errors({ stack: true }),
-      format.json(),
-    ),
-    transports: [
-      new transports.File({ filename: 'logs/error.log', level: 'error' }),
-      new transports.File({ filename: 'logs/combined.log' }),
-    ],
-  });
+  private readonly logger: WinstonLogger;
+
+  constructor() {
+    this.logger = createLogger({
+      format: format.combine(
+        format.timestamp(),
+        format.errors({ stack: true }),
+        format.json(),
+      ),
+      transports: [
+        new transports.File({ filename: 'logs/error.log', level: 'error' }),
+        new transports.File({ filename: 'logs/combined.log' }),
+      ],
+    });
+
+    if (process.env.NODE_ENV !== 'prod') {
+      this.logger.add(
+        new transports.Console({
+          format: format.combine(
+            format.colorize(),
+            format.printf(({ level, message, timestamp }) => {
+              return `[${timestamp}] ${level}: ${message}`;
+            }),
+          ),
+        }),
+      );
+    }
+  }
 
   log(message: string) {
     this.logger.info(message);
